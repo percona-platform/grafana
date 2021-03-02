02778d46c920acc12ce3dbb93cb9c347b129ab7c
Fabio Silva <ffjs1993@gmail.com>
2021-03-02 15:52:21 +0000

    PMM-7509 fix tests

--- public/app/features/integrated-alerting/components/Alerts/__mocks__/Alerts.service.ts
+++ public/app/features/integrated-alerting/components/Alerts/__mocks__/Alerts.service.ts
@@ -4,5 +4,6 @@ import * as alertsService from '../Alerts.service';
 
 export const AlertsService = jest.genMockFromModule<typeof alertsService>('../Alerts.service').AlertsService;
 
-AlertsService.list = () => Promise.resolve({ alerts: alertsStubs });
+AlertsService.list = () =>
+  Promise.resolve({ alerts: alertsStubs, totals: { total_pages: 1, total_items: alertsStubs.length } });
 AlertsService.toggle = () => Promise.resolve();
